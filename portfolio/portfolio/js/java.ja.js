bc444186c7b06ebc4ba00d00ded2c92604198a3f
koshiko1945 <m01161265d@edu.teu.ac.jp>
2018-01-13 16:41:24 +0900

    Add files via upload

---
+++ portfolio/portfolio/js/java.ja.js
@@ -0,0 +1,15 @@
+// 【各引数の定義】
+// idImage = 画像のID
+// imageName = 画像名
+// idLink = aタグのID
+// URL = リンク先URL
+
+function changeImg(idImage,imageName,idLink,URL) {
+    document.getElementById(image_01).src = "image/portfolio.png";
+    document.getElementById(link_id01).href = URL01;
+}
+
+function changeImg(image_id02,imageName,idLink,URL) {
+    document.getElementById(imageid_02).src = "image/port_osanpo.png";
+    document.getElementById(link_id01).href = URL02;
+}
